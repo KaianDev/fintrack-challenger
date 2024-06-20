33d5b7c8ed95e44bf44120558adc2f5669ddd5ee
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-06-19 22:36:57 -0300

    feat: Refactor transaction card and TransactionTitle component

--- app/dashboard/_components/transaction-card.tsx
+++ app/dashboard/_components/transaction-card.tsx
@@ -1,4 +1,4 @@
-import { LucideIcon } from "lucide-react"
+import { PropsWithChildren } from "react"
 
 // Utilities
 import { formatMoney } from "@/helpers/format-money"
@@ -6,30 +6,15 @@ import { formatMoney } from "@/helpers/format-money"
 // Components
 import { Card, CardContent } from "@/components/ui/card"
 
-interface TransactionCardProps {
-  label: string
+interface TransactionCardProps extends PropsWithChildren {
   value: number
-  icon: LucideIcon
-  color?: string
 }
 
-export const TransactionCard = ({
-  value,
-  label,
-  icon,
-  color,
-}: TransactionCardProps) => {
-  const Icon = icon
-
+export const TransactionCard = ({ value, children }: TransactionCardProps) => {
   return (
     <Card className="rounded-lg">
       <CardContent className="space-y-2 p-6">
-        <div className="flex items-center gap-2">
-          <div className="w-max rounded-lg bg-white/10 p-2">
-            <Icon size={16} className={color} />
-          </div>
-          <p className="text-sm text-muted">{label}</p>
-        </div>
+        {children}
         <p className="title">{formatMoney(value)}</p>
       </CardContent>
     </Card>
---
+++ app/dashboard/_components/transaction-title.tsx
@@ -0,0 +1,28 @@
+import { LucideIcon } from "lucide-react"
+
+// Utilities
+import { Colors } from "@/data/enum"
+import { cn } from "@/lib/utils"
+
+interface TransactionTitleProps {
+  icon: LucideIcon
+  label: string
+  color?: string
+}
+
+export const TransactionTitle = ({
+  icon,
+  label,
+  color,
+}: TransactionTitleProps) => {
+  const Icon = icon
+
+  return (
+    <div className="flex items-center gap-2">
+      <div className="w-max rounded-lg bg-white/10 p-2">
+        <Icon size={16} className={color ?? "text-white"} />
+      </div>
+      <p className="text-sm text-muted">{label}</p>
+    </div>
+  )
+}
