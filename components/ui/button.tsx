78ad2a0aa6253540582014a28507e67bf20eeaa8
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-06-22 14:52:35 -0300

    refactor: add cancel button variant to Button component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        cancel: "bg-[#1F1F21] text-white hover:bg-[#1F1F21]/70",
       },
       size: {
         default: "h-9 px-4 py-2",
@@ -31,7 +32,7 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  }
+  },
 )
 
 export interface ButtonProps
@@ -50,7 +51,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       />
     )
-  }
+  },
 )
 Button.displayName = "Button"
 
