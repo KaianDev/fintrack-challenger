c5565f02127fc4b4b02bbbffd1812fbf1ba047e1
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-06-27 15:07:15 -0300

    refactor: optimize getTransactionsToChartBars function

--- helpers/get-transactions-to-chart-bar.ts
+++ helpers/get-transactions-to-chart-bar.ts
@@ -2,34 +2,35 @@ import { TransactionData } from "@/app/dashboard/types"
 import { TransactionType } from "@prisma/client"
 import dayjs from "dayjs"
 
+
 export const getTransactionsToChartBars = (
   transactionData: TransactionData[],
 ) => {
-  const transactions = [] as {
-    date: Date
-    type: TransactionType
-    label: number | string
-    amount: number
-  }[]
-  for (let transaction of transactionData) {
-    const { date, amount } = transaction
+  const transactionsMap = new Map<
+    string,
+    {
+      date: Date
+      type: TransactionType
+      label: string
+      amount: number
+    }
+  >()
 
-    const tIndex = transactions.findIndex(
-      (t) =>
-        t.type === transaction.type && +t.date.getTime() === +date.getTime(),
-    )
-    if (tIndex > -1) {
-      transactions[tIndex].amount += amount
+  for (let transaction of transactionData) {
+    const { date, amount, type } = transaction
+    const key = `${type}-${date.getTime()}`
+
+    if (transactionsMap.has(key)) {
+      transactionsMap.get(key)!.amount += amount
     } else {
-      transactions.push({
+      transactionsMap.set(key, {
+        amount,
+        date,
+        type,
         label: dayjs(date).format("DD"),
-        amount: transaction.amount,
-        type: transaction.type,
-        date: transaction.date,
       })
     }
   }
-  return transactions.sort((a, b) => {
-    return +a.date.getDate() > +b.date.getDate() ? 1 : -1
-  })
+  const transactions = Array.from(transactionsMap.values())
+  return transactions.sort((a, b) => a.date.getTime() - b.date.getTime())
 }
